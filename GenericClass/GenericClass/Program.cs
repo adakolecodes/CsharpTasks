f3f24e0317ecc704c128f9490ab4c0f0435ced13
adakolecodes <amdeezy.com@gmail.com>
2022-02-19 10:27:39 +0100

    Update

--- GenericClass/GenericClass/Program.cs
+++ GenericClass/GenericClass/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace GenericClass
 {
+    public class DanielGeneric<DKey, DValue>
+    {
+        public DKey keyinput;
+
+        public void MyAddMethod(DKey dkey, DValue dvalue)
+        {
+            
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            var genericclass = new DanielGeneric<int, string>();
+            genericclass.MyAddMethod(1, "January");
+            genericclass.MyAddMethod(2, "February");
+            genericclass.MyAddMethod(3, "March");
+            genericclass.MyAddMethod(4, "April");
         }
     }
 }
