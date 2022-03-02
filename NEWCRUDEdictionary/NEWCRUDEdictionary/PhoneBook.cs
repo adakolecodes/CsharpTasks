5fd5698ecd64a00a8bef4594fc8ffa27d244744b
adakolecodes <amdeezy.com@gmail.com>
2022-03-02 17:57:07 +0100

    Adds

---
+++ NEWCRUDEdictionary/NEWCRUDEdictionary/PhoneBook.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEWCRUDEdictionary
+{
+    internal class PhoneBook
+    {
+        private Dictionary<string, long> book = new Dictionary<string, long>();
+        //ADD METHOD
+        public bool AddMethod(string name, long num)
+        {
+            //Check if key (contact name) already exists, if yes, then replace contact where key matches input, else add it
+            if (book.ContainsKey(name))
+            {
+                return false;
+            }
+            else
+            {
+                book.Add(name, num);
+                return true;
+            }
+        }
+
+        public bool GetMethod(string name)
+        {
+            if (book.ContainsKey(name))
+            {
+                Console.WriteLine(book[name]);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateMethod(string name, long num)
+        {
+            if (book.ContainsKey(name))
+            {
+                book[name] = num;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteMethod(string name)
+        {
+            if (book.ContainsKey(name))
+            {
+                book.Remove(name);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ NEWCRUDEdictionary/NEWCRUDEdictionary/Program.cs
@@ -0,0 +1,92 @@
+﻿
+using System;
+
+namespace NEWCRUDEdictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a new contact details. Eg STORE daniel 09034227044");
+            var phonebook = new PhoneBook();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                //STORE daniel 09034227044
+                var split = input.Split();
+
+                var command = split[0];
+                if (command == "STORE")
+                {
+                    var name = split[1];
+                    var num = split[2];
+                    var res = phonebook.AddMethod(name, long.Parse(num));
+                    if (res)
+                    {
+                        Console.WriteLine("Contact created successfully, contact is:");
+                        phonebook.GetMethod(name);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Contact exists already");
+                    }
+                }
+                else if (command == "VIEW")
+                {
+                    var name = split[1];
+                    var res = phonebook.GetMethod(name);
+                    if (res)
+                    {
+                        Console.WriteLine("Requested contact is:");
+                        phonebook.GetMethod(name);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Contact name does not exist in phone book");
+                    }
+                }else if (command == "UPDATE")
+                {
+                    var name = split[1];
+                    var num = split[2];
+                    if(num == "")
+                    {
+                        Console.WriteLine("Error: Kindly enter updated number");
+                    }
+                    else
+                    {
+                        var res = phonebook.UpdateMethod(name, long.Parse(num));
+                        if (res)
+                        {
+                            Console.WriteLine("Contact updated successfully, updated contact is:");
+                            phonebook.GetMethod(name);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Oops, sorry you cant update this contact because contact name cant be found");
+                        }
+                    }
+                }else if(command == "DELETE")
+                {
+                    var name = split[1];
+                    var res = phonebook.DeleteMethod(name);
+                    if (res)
+                    {
+                        Console.WriteLine("Contact deleted successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Contact name does not exist in phone book");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Command does not exist; command can either be STORE, VIEW, UPDATE or DELETE");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
