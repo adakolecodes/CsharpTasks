fe0166791b65ea44dd0a6b4f580bdbf5960c1213
adakolecodes <amdeezy.com@gmail.com>
2022-03-12 11:40:13 +0100

    Adds

---
+++ Collections LINQ Assignment/Collections LINQ Assignment/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_LINQ_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			var people = new List<Person>()
+			{
+				new Person("Bill", "Smith", 41),
+				new Person("Sarah", "Jones", 22),
+				new Person("Stacy","Baker", 21),
+				new Person("Vivianne","Dexter", 19 ),
+				new Person("Bob","Smith", 49 ),
+				new Person("Brett","Baker", 51 ),
+				new Person("Mark","Parker", 19),
+				new Person("Alice","Thompson", 18),
+				new Person("Evelyn","Thompson", 58 ),
+				new Person("Mort","Martin", 58),
+				new Person("Eugene","deLauter", 84 ),
+				new Person("Gail","Dawson", 19 ),
+			};
+
+			//QUESTION: write linq statement for all the people who are have the surname Thompson and Baker. Write all the first names to the console
+			//SOLUTION 2
+			var targets = people.Where(x => x.LastName == "Thompson" || x.LastName == "Baker");
+			//We have target list where everyone has the surname of either Thomson or Baker
+			List<Person> targetList = targets.ToList();
+			//Looping through our target list
+			foreach (Person mylist in targetList)
+			{
+				Console.WriteLine(mylist.FirstName);
+			}
+
+			//QUESTION: write linq statement for the people with last name that starts with the letter D
+			//SOLUTION 1
+			var targets2 = people.Where(y => y.LastName.Substring(0, 1) == "D");
+			List<Person> targetList2 = targets2.ToList();
+
+			//Get the count of the number of people who's last name starts with letter D
+			Console.WriteLine(targetList2.Count);
+
+			//Display the number of people who's last name starts with letter D
+			foreach (Person mylist2 in targetList2)
+			{
+				Console.WriteLine(mylist2.LastName);
+			}
+
+
+			Console.ReadKey();
+		}
+
+		public class Person
+		{
+			public Person(string firstName, string lastName, int age)
+			{
+				FirstName = firstName;
+				LastName = lastName;
+				Age = age;
+			}
+
+			public string FirstName { get; set; }
+			public string LastName { get; set; }
+			public int Age { get; set; }
+
+		}
+	}
+}
---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person()
+            {
+                Name = "Amos",
+                Age = 20
+            };
+            Person person2 = new Person()
+            {
+                Name = "Mike",
+                Age = 35
+            };
+            Person person3 = new Person()
+            {
+                Name = "Mercy",
+                Age = 27
+            };
+            Person person4 = new Person()
+            {
+                Name = "Amos",
+                Age = 12
+            };
+            List<Person> list = new List<Person>()
+            {
+                person1,
+                person2,
+                person3,
+                person4,
+            };
+
+            //Changing the age of every person in our list to 10 using for loop
+            for(int x = 0; x < list.Count; x++)
+            {
+                list[x].Age = 10;
+            }
+
+            //Using foreach. Foreach loop does not care about the index
+            foreach(Person currentPerson in list)
+            {
+                currentPerson.Age = 2;
+            }
+
+            //How to go through our list, find someone with the name Mercy and change the age to 50
+            foreach(Person currentPerson in list)
+            {
+                if(currentPerson.Name == "Mercy")
+                {
+                    currentPerson.Age = 50;
+                }
+            }
+
+            //LINQ select and where. We can use LINQ to achieve same last example above. We can use LINQ on anything that is IEnumerable - a list is one
+            //This creates a new list based on a syntax. It creates a new list of everyone who has the name of Mercy. (x below can be anything)
+            var targets = list.Where(x => x.Name == "Mercy");
+            //We have target list where everyone is just Mercy
+            List<Person> targetList = targets.ToList();
+            //Looping through our target list
+            foreach(Person mercys in targetList)
+            {
+                mercys.Age = 50;
+            }
+            //NOTE:
+            //We use .Where to filter things from a list to make a new list
+
+            //LINQ .Select
+            //What we get here is not people but just the list of the values of the length of their names
+            var selectTargets = list.Select(x => x.Name.Length);
+            foreach(var value in selectTargets)
+            {
+                Console.WriteLine(value);
+            }
+
+            //HOME WORK
+
+        }
+
+        class Person
+        {
+            public int Age;
+            public string Name;
+        }
+    }
+}
