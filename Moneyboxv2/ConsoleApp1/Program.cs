c66e1a483456d28b98aa53542ecd85c4b39f43b9
adakolecodes <amdeezy.com@gmail.com>
2022-05-26 17:30:39 +0100

    Version changed to 4.7.2 Ability to create new account

--- Moneyboxv2/ConsoleApp1/Program.cs
+++ Moneyboxv2/ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Moneybox.App;
+using Moneybox.App.Domain;
 
 namespace ConsoleApp1
 {
@@ -12,6 +13,33 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             var it = new Account();
+
+            while (true)
+            {
+                var instruction = Console.ReadLine();
+                //How to compare string to ignore case
+                if("create".Equals(instruction, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    Console.WriteLine("What is your name?");
+                    var name = Console.ReadLine();
+                    Console.WriteLine("What is your email?");
+                    var email = Console.ReadLine();
+
+                    var user = new User();
+                    user.Name = name;
+                    user.Email = email;
+
+                    var account = new Account();
+
+                    account.User = user;
+                    account.Id = 1;
+                    user.Id = 1;
+
+                    //Make the Id unique
+                    //Store the accounts in a list
+
+                }
+            }
         }
     }
 }
