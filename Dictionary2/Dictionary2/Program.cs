2bdd94316f1515c27e53dff350ded7f62121ea7b
adakolecodes <amdeezy.com@gmail.com>
2022-06-07 11:52:32 +0100

    added

---
+++ Dictionary2/Dictionary2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("THIS IS MY DICTIONARY");
+
+Dictionary<int, string> myHobbies = new Dictionary<int, string>();
+myHobbies.Add(1, "Singing");
+myHobbies.Add(2, "Programming");
+myHobbies.Add(3, "Photography");
+myHobbies.Add(4, "Music Production");
+
+foreach(var hobby in myHobbies)
+{
+    Console.WriteLine("My Hobby is: " + hobby.Value);
+}
+
+
+for(int i = 1; i <= myHobbies.Count; i++)
+{
+    Console.WriteLine("My Hobby is: " + myHobbies[i]);
+}
---
+++ DictionaryWithInput/DictionaryWithInput/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Dictionary<string, string> myPhoneBook = new Dictionary<string, string>();
+
+while (true)
+{
+    Console.WriteLine("What would you like to do? SAVE, VIEW, UPDATE, DELETE");
+    string action = Console.ReadLine();
+    if(action == "SAVE")
+    {
+        Console.WriteLine("Enter contact name");
+        string contactName = Console.ReadLine();
+        Console.WriteLine("Enter contact phone number");
+        string contactPhone = Console.ReadLine();
+
+        myPhoneBook.Add(contactName, contactPhone);
+
+        Console.WriteLine(myPhoneBook[contactName] + " added successfully");
+    }
+    else if (action == "VIEW")
+    {
+        Console.WriteLine("Enter contact name to view number");
+        string contactName = Console.ReadLine();
+        Console.WriteLine("Number is: " + myPhoneBook[contactName]);
+    }
+}
