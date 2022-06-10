1ac52d5bc05288d708a3ebf8d9c528f7006b5173
adakolecodes <amdeezy.com@gmail.com>
2022-06-10 12:52:53 +0100

    Updated

--- QuizApp/QuizApp/Program.cs
+++ QuizApp/QuizApp/Program.cs
@@ -1,46 +1,61 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Dictionary<string, string> questions = new Dictionary<string, string>();
+bool finished = false;
 
-questions.Add("4", "How many days are in a week?");
-questions.Add("60", "How many seconds are in a minute?");
-questions.Add("24", "How many hours in a day?");
-
-
-while (true)
+while (finished == false)
 {
     int grade = 0;
+    int totalgrade = 0;
 
-    Console.WriteLine(questions["4"]);
-    Console.WriteLine("2");
-    Console.WriteLine("4");
-    Console.WriteLine("5");
-    Console.WriteLine("20");
-    string answer = Console.ReadLine();
-    if(answer == "4")
+    SetQuestions("How many days are in a week?", "7", "6", "8", "2");
+    string optionQ1 = Console.ReadLine();
+    if (optionQ1 == "A")
     {
-        Console.WriteLine("Pass");
-        grade++;
-        Console.WriteLine("You have gotten " + grade + " point");
+        Console.WriteLine("PASS! You have gotten 1 point");
+        totalgrade++;
     }
     else
     {
-        Console.WriteLine("Fail");
+        Console.WriteLine("FAILED");
     }
-    Console.WriteLine(questions["60"]);
-    Console.WriteLine("2");
-    Console.WriteLine("4");
-    Console.WriteLine("5");
-    Console.WriteLine("20");
-    string answer2 = Console.ReadLine();
-    if (answer == "4")
+
+    SetQuestions("How many seconds are in a minute?", "20", "60", "50", "12");
+    string optionQ2 = Console.ReadLine();
+    if (optionQ2 == "B")
     {
-        Console.WriteLine("Pass");
-        grade++;
-        Console.WriteLine("You have gotten " + grade + " point");
+        Console.WriteLine("PASS! You have gotten 1 point");
+        totalgrade++;
     }
     else
     {
-        Console.WriteLine("Fail");
+        Console.WriteLine("FAILED");
     }
+
+    SetQuestions("How many hours in a day?", "90", "12", "34", "24");
+    string optionQ3 = Console.ReadLine();
+    if (optionQ3 == "D")
+    {
+        Console.WriteLine("PASS! You have gotten 1 point");
+        totalgrade++;
+    }
+    else
+    {
+        Console.WriteLine("FAILED");
+    }
+
+    Console.WriteLine("YOUR RESULT IS: " + totalgrade);
+
+    finished = true;
+}
+
+
+
+static void SetQuestions(string question, string option1, string option2, string option3, string option4)
+{
+    Console.WriteLine(question);
+    Console.WriteLine("A)" + option1);
+    Console.WriteLine("B)" + option2);
+    Console.WriteLine("C)" + option3);
+    Console.WriteLine("D)" + option4);
+    Console.Write("Enter option: ");
 }
