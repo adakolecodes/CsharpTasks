40229f5437b226c3e4984061dd44f8e2af4010ac
adakolecodes <amdeezy.com@gmail.com>
2022-06-19 21:35:53 +0100

    Int

---
+++ FieldsAndProperties/FieldsAndProperties/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldsAndProperties
+{
+    internal class Car
+    {
+        private int speed;
+
+        public Car(int aSpeed)
+        {
+            SpeedProperty = aSpeed;
+        }
+
+        public int SpeedProperty
+        {
+            get { return speed; } //Read
+            set {                 //Writable
+                if(value > 500)
+                {
+                    speed = 500;
+                }
+                else
+                {
+                    speed = value;
+                }
+            }
+        }
+    }
+}
---
+++ FieldsAndProperties/FieldsAndProperties/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldsAndProperties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(200);
+            car.SpeedProperty = 6000;
+
+            Console.WriteLine(car.SpeedProperty);
+
+            Console.ReadKey();
+        }
+    }
+}
