50b8118fdf5c298b8428cc1e4a86b987d792395f
adakolecodes <amdeezy.com@gmail.com>
2022-06-22 00:15:00 +0100

    Init

---
+++ AccessModifiers/AccessModifiers/Human.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    internal class Human
+    {
+        //Public modifier
+        public int eyes;
+        //Private modifier
+        private int legs;
+        //Internal modifier
+        internal string color;
+    }
+}
---
+++ AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human1 = new Human();
+
+            //Accessing the various modifiers
+
+            //We can access eyes because the field is declared public
+            human1.eyes = 2;
+
+            //We cannot access legs because the field is declared private
+            human1.legs = 2;
+
+            //We can access color as an internal field because both the
+            //Program class and Human class belong to the same assembly
+            human1.color = "Chocolate";
+        }
+    }
+}
