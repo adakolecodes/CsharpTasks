24e8fca4701419b0d5cc162ef445b5c0854e6e83
adakolecodes <amdeezy.com@gmail.com>
2022-07-12 12:12:58 +0100

    Iniit

---
+++ Debugging/Debugging/Message.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debugging
+{
+    internal class Message
+    {
+        public static string ShowMessage(string lang)
+        {
+            if(lang == "english")
+            {
+                return "Come";
+            }
+            else if (lang == "yoruba")
+            {
+                return "Wa";
+            }
+            else if (lang == "hausa")
+            {
+                return "Zo";
+            }
+            else if (lang == "igbo")
+            {
+                return "Bia";
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
---
+++ Debugging/Debugging/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debugging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string call = Message.ShowMessage("english");
+            string call2 = Message.ShowMessage("igbo");
+            Console.WriteLine(call);
+            Console.WriteLine(call2);
+
+            Console.ReadLine();
+        }
+    }
+}
