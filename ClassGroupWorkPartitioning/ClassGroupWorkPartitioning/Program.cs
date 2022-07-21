7cb17da59011d4cb8e91472e4e982a07a96f8253
adakolecodes <amdeezy.com@gmail.com>
2022-07-21 10:04:13 +0100

    init

---
+++ ClassGroupWorkPartitioning/ClassGroupWorkPartitioning/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassGroupWorkPartitioning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = { 10,33,6,7,8,9,10,3, 4, 5, };
+
+            var MethodSyntax = numbers.Take(4).ToList();
+
+            var ms = numbers.TakeWhile(num=>num<33).ToList();
+
+ 
+
+            Console.ReadLine();
+        }
+    }
+}
