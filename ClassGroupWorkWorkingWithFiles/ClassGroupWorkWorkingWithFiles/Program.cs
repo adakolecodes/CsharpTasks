8f486c9d292934ec3cf49f19f4837fd6bdb3f016
adakolecodes <amdeezy.com@gmail.com>
2022-07-25 14:15:04 +0100

    Init

---
+++ ClassGroupWorkWorkingWithFiles/ClassGroupWorkWorkingWithFiles/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ClassGroupWorkWorkingWithFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\danie\source\repos\Documents\";
+            string[] dirs = Directory.GetFiles(path);
+            string destinationPath = @"C:\Users\danie\source\";
+
+            foreach(string d in dirs)
+            {
+                File.Copy(d, $"{destinationPath}{Path.GetFileName(d)}", true);
+            }
+        }
+    }
+}
