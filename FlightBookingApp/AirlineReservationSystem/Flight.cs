5643a8b5571ab7dee376fa02a0c60a08be6e4e0c
adakolecodes <amdeezy.com@gmail.com>
2022-08-17 10:13:48 +0100

    Init and refactoring

---
+++ FlightBookingApp/AirlineReservationSystem/Flight.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineReservationSystem
+{
+    internal class Flight
+    {
+        public Flight()
+        {
+            Passengers = new List<Passenger>();
+        }
+        public int FlightNumber { get; set; }
+        public string Takeoff { get; set; }
+        public string Destination { get; set; }
+        public DateTime Date { get; set; }
+        public List<Passenger> Passengers { get; set; }
+        public Plane Plane { get; set; }
+    }
+}
