4d3744a15ac4167e97918b0eecb6b4d9f67e3a45
adakolecodes <amdeezy.com@gmail.com>
2022-08-18 11:42:19 +0100

    New project

---
+++ SearchEngineApp/SearchEngineApp/Place.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchEngineApp
+{
+    internal class Place
+    {
+        public string Town { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+    }
+}
