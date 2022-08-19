5eb7de461c10bd58f93a5d8ac935c61dfda5377a
adakolecodes <amdeezy.com@gmail.com>
2022-08-19 14:29:36 +0100

    StudentsWork

---
+++ ClassGroupWorkAirlineReservationSystem2/ClassGroupWorkAirlineReservationSystem2/AirlineService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassGroupWorkAirlineReservationSystem2
+{
+    internal class AirlineService
+    {
+        List<Plane> plane;
+        List<Flight> flight;
+        Dictionary<long, Passenger> passenger;
+
+
+         public AirlineService()
+        {
+            plane = new List<Plane>()
+            {
+                new Plane(){Name = "Emirates Airline", AirCraft_Number = 100023, NumberOfSeat = 700},
+                new Plane(){Name = "Nigeria Fly", AirCraft_Number = 100033, NumberOfSeat = 600},
+                new Plane(){Name = "The Eagle", AirCraft_Number = 100043, NumberOfSeat = 400}
+            };
+
+             flight = new List<Flight>();
+             passenger = new Dictionary<long, Passenger>();
+        }
+
+        public void RegisterAPassenger(long phonenumber, string firstName, string lastName)
+        {
+            var passengers = new Passenger() { First_Name=firstName, Last_Name=lastName, Passport_No=phonenumber};
+            passenger.Add(phonenumber, passengers);
+            
+        }
+        public void AddAFlight(long flightNumber,string takeOff,string destination,string dateOfFlight,long aircraftNumber)
+        {
+            var planes = plane.Where(x => x.AirCraft_Number == aircraftNumber).First();
+            if (planes == null)
+            {
+                throw new Exception();
+            }
+            else
+            {
+                var flights = new Flight()
+                {
+                    Flight_Number = flightNumber,
+                    TakeOff = takeOff,
+                    Destination = destination,
+                    Date_Of_Flight = dateOfFlight,
+                    Planes = planes
+                };
+
+                flight.Add(flights);
+            }
+        }
+        public void AddAPassengerToAFlight(long passportNumber, long flightNumber)
+        {
+            var flights = flight.Where(x => x.Flight_Number == flightNumber).FirstOrDefault();
+            var passengers = passenger[passportNumber];
+            if(flights==null)
+            {
+                throw new Exception();
+            }
+            else
+            {
+                flights.Passengers.Add(passengers);
+            }
+        }
+
+    }
+}
---
+++ ClassGroupWorkAirlineReservationSystem2/ClassGroupWorkAirlineReservationSystem2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassGroupWorkAirlineReservationSystem2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                AirlineService airlineService = new AirlineService();
+                Console.WriteLine("[1] Add A Passenger.[2] Add A Flight. [3] Add a passenger to a flight.");
+                string op = Console.ReadLine();
+                if (op == "1")
+                {
+                    Console.WriteLine("ENTER THE FIRST NAME OF THE PASSENGER");
+                    var firstName = Console.ReadLine();
+                    Console.WriteLine("ENTER THE LAST NAME OF THE PASSENGER");
+                    var lastName = Console.ReadLine();
+                    Console.WriteLine("ENTER THE PASSPORT NUMBER OF THE PASSENGER");
+                    try
+                    {
+                        var phoneNumber = long.Parse(Console.ReadLine());
+                        airlineService.RegisterAPassenger(phoneNumber, firstName, lastName);
+                        Console.WriteLine("THE PASSENGER HAS BEEN SUCCESSFULLY REGISTERED");
+                    }
+                    catch (Exception err)
+                    {
+                        Console.WriteLine(err.Message);
+                    }
+                }
+                else if (op == "2")
+                {
+                    Console.WriteLine("ENTER THE FLIGHT NUMBER");
+                    var flightnumber = long.Parse(Console.ReadLine());
+                    Console.WriteLine("DATE OF THE FLIGHT");
+                    var dateofflight = Console.ReadLine();
+                    Console.WriteLine("TAKE OFF");
+                    var takeoff = Console.ReadLine();
+                    Console.WriteLine("DESTINATION");
+                    var destination = Console.ReadLine();
+                    Console.WriteLine("Enter Aircraft Number");
+                    var aircraftNumber = long.Parse(Console.ReadLine());
+                    airlineService.AddAFlight(flightnumber, dateofflight, destination, takeoff, aircraftNumber);
+                    Console.WriteLine("FLIGHT ADDED SUCCESSFULLY");
+
+                }
+                else if (op == "3")
+                {
+
+                    Console.WriteLine("ENTER THE PASSPORT NUMBER OF THE PASSENGER");
+                    var passportnumber = long.Parse(Console.ReadLine());
+
+                    Console.WriteLine("ENTER THE FLIGHT NUMBER YOU BOOKED FOR");
+                    var flightnumber = long.Parse(Console.ReadLine());
+
+                    airlineService.AddAPassengerToAFlight(passportnumber, flightnumber);
+                    Console.WriteLine("PASSENGER HAVE BEEN ADDED TO THE FLIGHT.");
+                }
+
+            }
+        }
+    }
+}
