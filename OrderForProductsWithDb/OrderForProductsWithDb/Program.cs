0559f95d3b3c269ed73e7bfdd5397f9dfc2ff61f
adakolecodes <amdeezy.com@gmail.com>
2022-08-25 11:58:47 +0100

    More code

--- OrderForProductsWithDb/OrderForProductsWithDb/Program.cs
+++ OrderForProductsWithDb/OrderForProductsWithDb/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OrderForProductsWithDb.Data;
+using OrderForProductsWithDb.Models;
+
+using OrderForProductsContext context = new OrderForProductsContext();
+
+Product laptop = new Product()
+{
+    Name = "HP Pavillion 15",
+    Price = 420M
+};
+
+context.Products.Add(laptop);
+
+Product phone = new Product()
+{
+    Name = "Samsung Galaxy A9 Star",
+    Price = 190M
+};
+
+context.Products.Add(phone);
+
+context.SaveChanges();
