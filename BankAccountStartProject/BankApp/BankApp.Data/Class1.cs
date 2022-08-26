f517bb1b6ed2214430a019d71a9cbc6ec3cbd0fb
adakolecodes <amdeezy.com@gmail.com>
2022-08-26 22:33:50 +0100

    Added class library

---
+++ BankAccountStartProject/BankApp/BankApp.Data/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace BankApp.Data
+{
+    public class Class1
+    {
+        //Scaffold-DbContext "data source=(LocalDB)\MSSQLLocalDB;initial catalog=Bank2;MultipleActiveResultSets=True;App=EntityFramework" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Scaffolded
+    }
+}
