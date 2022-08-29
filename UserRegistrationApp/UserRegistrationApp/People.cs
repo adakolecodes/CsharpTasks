412117fad7fafe8c1af1a07b5abbfe561d40693d
adakolecodes <amdeezy.com@gmail.com>
2022-08-29 12:02:35 +0100

    More codes

---
+++ UserRegistrationApp/UserRegistrationApp/People.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserRegistrationApp.Data.Scaffolded;
+
+namespace UserRegistrationApp
+{
+    public class People
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+
+        public int CreateAccount(string name, string email)
+        {
+            var dbUser = new User();
+            dbUser.Name = name;
+            dbUser.Email = email;
+
+            using (var dbContext = new UserRegistrationContext())
+            {
+                var result = dbContext.Users.SingleOrDefault(x => x.Email == email);
+                if (result != null)
+                {
+                    return 0;
+                }
+                else
+                {
+                    dbContext.Users.Add(dbUser);
+                    dbContext.SaveChanges();
+                }
+            }
+            return dbUser.Id;
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            using (var dbContext = new UserRegistrationContext())
+            {
+                return dbContext.Users.Select(x => x).ToList();
+            }
+        }
+
+        public User GetUserByEmail(string email)
+        {
+            using (var dbContext = new UserRegistrationContext())
+            {
+                var result = dbContext.Users.SingleOrDefault(x => x.Email == email);
+
+                var user = new User();
+                user.Name = result.Name;
+                user.Email = result.Email;
+
+                return user;
+            }
+        }
+
+        public
+    }
+}
--- UserRegistrationApp/UserRegistrationApp/Program.cs
+++ UserRegistrationApp/UserRegistrationApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using UserRegistrationApp;
 using UserRegistrationApp.Data.Scaffolded;
 
 namespace UserRegistration
@@ -8,11 +9,64 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
-            using (var dbContext = new UserRegistrationContext())
+            //using (var dbContext = new UserRegistrationContext())
+            //{
+            //    var user = new User(){ Name = "Daniel", Email = "daniel@gmail.com"};
+            //    dbContext.Users.Add(user);
+            //    dbContext.SaveChanges();
+            //}
+
+            var pple = new People();
+
+            while (true)
             {
-                var user = new User(){ Name = "Daniel", Email = "daniel@gmail.com"};
-                dbContext.Users.Add(user);
-                dbContext.SaveChanges();
+                Console.WriteLine(@"
+1 to add user,
+2 to view users
+3 to update user
+4 to delete user");
+
+                var option = Console.ReadLine();
+
+                if (option == "1")
+                {
+                    Console.Write("Enter Name: ");
+                    var name = Console.ReadLine();
+                    Console.Write("Enter Email: ");
+                    var email = Console.ReadLine();
+                    var user = pple.CreateAccount(name, email);
+                    if(user == 0)
+                    {
+                        Console.WriteLine("User already exists");
+                    }
+                    else
+                    {
+                        Console.WriteLine("User registered successfully");
+                    }
+                    
+                }else if (option == "2")
+                {
+                    Console.WriteLine("1 to view all users, 2 to view specific user");
+                    var option2 = Console.ReadLine();
+
+                    if(option2 == "1")
+                    {
+                        var allUsers = pple.GetAllUsers();
+
+                        foreach (var user in allUsers)
+                        {
+                            Console.WriteLine($"Name: {user.Name}, Email: {user.Email}");
+                        }
+                    }else if (option2 == "2")
+                    {
+                        Console.Write("Provide user email: ");
+                        var email = Console.ReadLine();
+
+                        var user = pple.GetUserByEmail(email);
+
+                        Console.WriteLine($"Name: {user.Name}, Email: {user.Email}");
+                    }
+                }
             }
         }
     }
